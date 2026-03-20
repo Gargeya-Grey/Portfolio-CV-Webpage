3fdb0d139e44cc628765c47a964de05fab2b92fd
Gargeya-Grey <gargeyablk16@gmail.com>
2026-03-20 21:50:01 +0530

    chore: configure OpenNext for Cloudflare Workers Builds

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  trailingSlash: true,
   reactCompiler: true,
 };
 
 export default nextConfig;
+
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
